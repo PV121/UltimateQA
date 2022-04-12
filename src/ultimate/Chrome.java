5e6edb8a0f2b399f424975f2a8cdce49434a3010
PV121 <vpinakin96@gmail.com>
2022-04-12 16:42:55 -0400

    ultimateQA

---
+++ src/ultimate/Chrome.java
@@ -0,0 +1,39 @@
+package ultimate;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Chrome {
+    public static void main(String[] args) {
+        String baseUrl= "https://courses.ultimateqa.com/users/sign_in";
+        System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get(baseUrl);
+
+        driver.manage().window().maximize();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+        String title = driver.getTitle();
+        System.out.println("Titlte : "+title);
+
+        String currentUrl = driver.getCurrentUrl();
+        System.out.println("Current URL: "+currentUrl);
+
+        String source = driver.getPageSource();
+        System.out.println("Page source: "+source);
+
+        WebElement emailfield = driver.findElement(By.id("user[email]"));
+        emailfield.sendKeys("pvaland@gmail.com");
+
+        WebElement passwordfield = driver.findElement(By.name("user[password]"));
+        passwordfield.sendKeys("123456789");
+
+        driver.close();
+    }
+}
